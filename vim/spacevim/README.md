243466d8f721ccbece1350595c98deab66b06525
Mark Paxson <kettleofketchup@gmail.com>
2018-04-12 08:50:18 -0400

    added stuff for spacevim

---
+++ vim/spacevim/README.md
@@ -0,0 +1,5 @@
+# Offline 
+
+After moving everything over to the host (`~/.SpaceVim`, `~/.SpaceVim.d/`, `~/.cache/vimfiles/`)
+  run the command 
+`ln -s "$HOME/.SpaceVim" "$HOME/.vim"`
--- vim/spacevim/copyfiles.sh
+++ vim/spacevim/copyfiles.sh
@@ -1,3 +1,20 @@
-tar -czf $PWD/vimfiles.tar.gz ~/.cache/vimfiles --absolute-names  
+#!/bin/bash -x
+thisdir=$PWD
+echo "this is primarily to install on a computer that is not online\n"
+echo "This dir is $thisdir"
+cd /home/`whoami`/.cache
+tar -czf vimfiles.tar.gz vimfiles/
+mv vimfiles.tar.gz $thisdir
+#go in home dir
+echo "CD to $HOME "
+
+cd $HOME
+echo "$PWD"
+tar -czvf .SpaceVim.tar.gz .SpaceVim/
+mv .SpaceVim.tar.gz $thisdir
+tar -czvf .SpaceVim.d.tar.gz .SpaceVim.d/
+mv .SpaceVim.d.tar.gz $thisdir
+cd $thisdir
 cp ~/.SpaceVim.d/init.vim init.vim
+
 
