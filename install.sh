e71f22deb0f52bdf074a2cef87c5ba2f00dede0f
kettleofketchup@gmail.com <kettleofketchup@gmail.com>
2025-07-21 18:23:47 -0400

    addes zsh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo apt install ruby-full curl git build-essential stow -y
+sudo apt install ruby-full curl git build-essential stow zsh -y
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
@@ -13,3 +13,4 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 # Install autoenv
 wget --show-progress -o /dev/null -O- 'https://raw.githubusercontent.com/hyperupcall/autoenv/main/scripts/install.sh' | sh
+
