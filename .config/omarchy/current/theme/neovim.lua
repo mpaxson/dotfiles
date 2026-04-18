d0764100f762978b62a89e67b6cfc65aaa93e723
KettleOfKetchup <kettleofketchup@gmail.com>
2026-04-18 06:53:35 -0400

    added omarchy config

--- .config/omarchy/current/theme/neovim.lua
+++ .config/omarchy/current/theme/neovim.lua
@@ -1,13 +1,12 @@
 return {
 	{
-		"bjarneo/hackerman.nvim",
-		dependencies = { "bjarneo/aether.nvim" }, -- Ensure aether is loaded first
+		"folke/tokyonight.nvim",
 		priority = 1000,
 	},
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "hackerman",
+			colorscheme = "tokyonight-night",
 		},
 	},
 }
