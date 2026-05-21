71151d5aa4cae177bf9e7fb8c5ff7b8b89acdfcd
KettleOfKetchup <kettleofketchup@gmail.com>
2026-05-21 09:14:45 -0400

    updated omarchy

--- .config/omarchy/current/theme/neovim.lua
+++ .config/omarchy/current/theme/neovim.lua
@@ -1,12 +1,12 @@
 return {
-	{
-		"folke/tokyonight.nvim",
-		priority = 1000,
-	},
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "tokyonight-night",
-		},
-	},
+  {
+    "bjarneo/ethereal.nvim",
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "ethereal",
+    },
+  },
 }
